e97c187d9dc9dffa43ab0d3c4424f4d22c22c1d2
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2020-11-11 11:18:11 +0100

    Initialdaten ergänzt

--- src/main/java/de/ars/demo/control/Fahrzeugbestand.java
+++ src/main/java/de/ars/demo/control/Fahrzeugbestand.java
@@ -3,6 +3,7 @@ package de.ars.demo.control;
 import java.util.Collection;
 import java.util.LinkedList;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 
 import de.ars.demo.entity.Fahrzeug;
@@ -13,6 +14,14 @@ public class Fahrzeugbestand {
 	private final Collection<Fahrzeug> fahrzeuge = new LinkedList<>();
 
 	// nur 1 Bestand in der Anwendung -> Singleton
+	
+	@PostConstruct
+	public void init() {
+		fahrzeuge.add(new Fahrzeug(1, "Ford", 2010));
+		fahrzeuge.add(new Fahrzeug(2, "Opel", 2018));
+		fahrzeuge.add(new Fahrzeug(3, "VW", 2015));
+		fahrzeuge.add(new Fahrzeug(4, "Renault", 1999));
+	}
 
 	public Collection<Fahrzeug> getFahrzeuge() {
 		return fahrzeuge;
